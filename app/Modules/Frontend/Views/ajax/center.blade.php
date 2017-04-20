572d738b28ea1b1bfed6826874f3365ec55d0556
Liem Phan <minhliemphp@gmail.com>
2017-04-20 10:34:52 +0700

    update center ajax

--- app/Modules/Frontend/Views/ajax/center.blade.php
+++ app/Modules/Frontend/Views/ajax/center.blade.php
@@ -1,5 +1,7 @@
 @if($list_center)
   {!!Form::select('id_center',[''=>'Chọn trung tâm ILA']+$list_center,old('id_center'), ['class'=>'form-control','id'=>'id_center'] )!!}
 @else
-  <option value="">Không có trung tâm ILA</option>
+<select class="form-control col-sm-6" name="id_center" id="id_center">
+    <option value="">Chọn trung tâm</option>
+</select>
 @endif
